8ed1be505c295a1906698c2b23e81537b5e0da26
mahuena <ads23a00024y@ait.edu.gh>
2024-06-18 15:33:49 +0000

    adding search bar

--- frontend1/src/components/NavBar.jsx
+++ frontend1/src/components/NavBar.jsx
@@ -1,7 +1,20 @@
-import { Container, Text, Flex, Box, Spacer } from '@chakra-ui/react';
+import { Container, Text, Flex, Box, Spacer, Input, InputGroup, InputRightElement } from '@chakra-ui/react';
 import { CreateContactModal } from './CreateContactModal';
+import { SearchIcon } from '@chakra-ui/icons';
+// import { useState, useEffect } from 'react';
 
-const Navbar = ({setUsers, getUsers}) => {
+const Navbar = ({
+    setUsers,
+    getUsers,
+    handleChangeImg,
+    handleDeleteImg,
+    image,
+    handleCreateContact,
+    isLoading,
+    inputs,
+    setInputs,
+    setFilteredUserInput
+}) => {
     return (
         <>
             <Container maxW={'12000px'}>
@@ -11,9 +24,31 @@ const Navbar = ({setUsers, getUsers}) => {
                             <Text fontSize='4xl' color={'#318CE7'}>Contacts</Text>
                         </Box>
                         <Spacer />
-                        <Box>
-                            <CreateContactModal setUsers={setUsers} getUsers={getUsers} />
-                        </Box>
+                        <Flex gap={4}>
+                            <InputGroup>
+                                <Input type='search-bar' placeholder='Search contacts' w={'300px'} _focusVisible={{border: '1px solid #ddd'}}
+                                       onChange={(e) => setFilteredUserInput(e.target.value)}
+                                />
+                                <InputRightElement>
+                                    <Container bg={"transparent"} mr={"4"}><SearchIcon /></Container>
+                                </InputRightElement>
+                            </InputGroup>
+
+                            <Spacer />
+                            <Box>
+                                <CreateContactModal
+                                    setUsers={setUsers}
+                                    getUsers={getUsers}
+                                    handleChangeImg={handleChangeImg}
+                                    handleDeleteImg={handleDeleteImg}
+                                    image={image}
+                                    handleCreateContact={handleCreateContact}
+                                    isLoading={isLoading}
+                                    inputs={inputs}
+                                    setInputs={setInputs}
+                                />
+                            </Box>
+                        </Flex>
                     </Flex>
                 </Box>
             </Container>
